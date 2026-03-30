a2bd83de36cfc0634e4203036bdee66de68f2c1c
Janhavi1214 <1680.janhavi@gmail.com>
2026-03-30 19:43:54 +0530

    Added dependencies and database

--- Order_matching_engine/src/main/java/com/trading/order_matching_engine/OrderMatchingEngineApplication.java
+++ Order_matching_engine/src/main/java/com/trading/order_matching_engine/OrderMatchingEngineApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderMatchingEngineApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMatchingEngineApplication.class, args);
+
+          SpringApplication.run(OrderMatchingEngineApplication.class, args);
     }
 
 }
